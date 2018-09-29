16683fca6d6286fc9630a3b973105ac7191ea37a
yujiawei <34087593@qq.com>
2018-09-29 13:05:44 +0800

    hello_selenium.py added

--- ch02/hello_selenium.py
+++ ch02/hello_selenium.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+
+import sys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Safari()
+
+driver = webdriver.Chrome()
 driver.get("http://cn.bing.com/")
 driver.find_element_by_id("sb_form_q").send_keys("selenium")
 driver.find_element_by_id("sb_form_go").click()
-# driver.find_element(by=By.ID, "sb_form_q")
+#driver.find_element(by=By.ID, "sb_form_q")
